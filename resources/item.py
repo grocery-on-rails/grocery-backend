916d737c1870dc0725154868675ca4415c75bca6
HorusXXVI <HorusXXVI@gmail.com>
2021-04-22 16:47:04 +0400

    Added discounted products on the homepage

--- resources/item.py
+++ resources/item.py
@@ -16,7 +16,7 @@ class ItemsApi(Resource):
         all_products = Product.objects()
         recently_viewed = extract_basic_info(json.loads(all_products[0:5].to_json()))
         new_arrivals = extract_basic_info(json.loads(Product.objects()[5:10].to_json()))
-        today_deals = extract_basic_info(json.loads(Product.objects(discount__gt=0).to_json()))
+        today_deals = extract_basic_info(json.loads(Product.objects(discount__gt=0)[:20].to_json()))
         top_sells = extract_basic_info(json.loads(Product.objects()[20:25].to_json()))
         fresh_vegies = extract_basic_info(json.loads(Product.objects()[0:16].to_json()))
         data = {}
