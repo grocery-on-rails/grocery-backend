7018e9f5a1a71257c8a3ea961a9868f1a1e8344b
HorusXXVI <HorusXXVI@gmail.com>
2021-04-24 18:43:19 +0400

    Added sort and filter

--- resources/item.py
+++ resources/item.py
@@ -103,48 +103,63 @@ class ItemSearchApi(Resource):
             isAscending = False
         else:
             isSortByPrice = False
-
-        price_min = price_max = 0
+            isAscending = False
+        match={"$match":{}}
+        price_min  = 0
+        price_max = 999999
         if body.get('price_min'):
             price_min = int(body.get('price_min'))
+            match["$match"]["price"]={"$gte":price_min}
         if body.get('price_max'):
             price_max = int(body.get('price_max'))
+            match["$match"]["price"]={"$lte":price_max}
         
         list_subcategory = []
         if body.get('subcategories'):
             list_subcategory = body.get('subcategories')
+            match["$match"]["subcategory"]={"$all":list_subcategory}
+
 
         keyword = unquote(raw_keyword)
         pipeline= [  
                     {"$search": {"text": {"query": keyword, "path": ["name","subcategory","description","others.quantity"]}}},
-                    {"$sort":{"score": -1}},
+                    
+                    {"$sort":{"score"*(1-int(isSortByPrice))+"price"*(int(isSortByPrice)): -1+2*(int(isAscending))}},
                     #{"$project": {"_id":0,"name": 1,"price": 1,"score": { "$meta": "searchScore" }}}
                   ]
+        if len(match["$match"])!=0:
+            pipeline.insert(1,match)
+        print(pipeline)
         matching_products = extract_basic_info((list(Product.objects().aggregate(pipeline))), isAdmin)[:100]
         ids=[str(i["_id"]) for i in matching_products]
        
         if len(matching_products)<10:
-                p= extract_basic_info( json.loads(Product.objects( name__istartswith=keyword).to_json()), isAdmin)
+                kwargs = dict(name__istartswith=keyword, price__gte=price_min,price__lte=price_max, subcategory__all=list_subcategory)
+
+                p= extract_basic_info( json.loads(Product.objects(**{k: v for k, v in kwargs.items() if v !=  []} ).to_json()), isAdmin)
                 
                 for i in p[:75]:             
                     if i["_id"]["$oid"] not in ids:
                             matching_products.append(i)
                             ids.append(i["_id"])
         if len(matching_products)<20:
-                p= extract_basic_info( json.loads(Product.objects( name__icontains=keyword).to_json()), isAdmin)
-               
-                for i in p[:50]:             
-                    if i["_id"]["$oid"] not in ids:
-                            matching_products.append(i)
-                            ids.append(i["_id"])
+            kwargs = dict(name__icontains=keyword, price__gte=price_min,price__lte=price_max, subcategory__all=list_subcategory)
+            p= extract_basic_info( json.loads(Product.objects( **{k: v for k, v in kwargs.items() if v != []}).to_json()), isAdmin)
+           
+            for i in p[:50]:             
+                if i["_id"]["$oid"] not in ids:
+                        matching_products.append(i)
+                        ids.append(i["_id"])
         if len(matching_products)<30:
-                p= extract_basic_info( json.loads(Product.objects( description__icontains=keyword).to_json()), isAdmin)
-                
-                for i in p[:25]:             
-                    if i["_id"]["$oid"] not in ids:
-                            matching_products.append(i)
-                            ids.append(i["_id"])
-        #matching_products= sorted(matching_products, key=lambda k: k['name'],reverse=True) 
+            kwargs = dict(description__icontains=keyword, price__gte=price_min,price__lte=price_max, subcategory__all=list_subcategory)
+            p= extract_basic_info( json.loads(Product.objects(**{k: v for k, v in kwargs.items() if v != []}).to_json()), isAdmin)
+            
+            for i in p[:25]:             
+                if i["_id"]["$oid"] not in ids:
+                        matching_products.append(i)
+                        ids.append(i["_id"])
+        if isSortByPrice:
+            matching_products= sorted(matching_products, key=lambda k: k['price'],reverse=not isAscending) 
         
         
         #matching_products = extract_basic_info(json.loads(Product.objects(name__contains=keyword).to_json()))
